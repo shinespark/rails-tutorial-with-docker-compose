470d16f3ce2e6d49770f2833057114e497b8a6b2
shinespark <shinespark64@gmail.com>
2016-06-15 03:06:56 -0700

    create user schema

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -11,17 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160612000542) do
+ActiveRecord::Schema.define(version: 20160615100542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "microposts", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -29,5 +22,7 @@ ActiveRecord::Schema.define(version: 20160612000542) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
