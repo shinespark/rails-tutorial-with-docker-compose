fc0c30b6bd527ba7be6069c5d4fb66845606fe2e
shinespark <shinespark64@gmail.com>
2016-06-15 19:30:29 -0700

    finish login

--- myapp/app/helpers/sessions_helper.rb
+++ myapp/app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+  # 渡されたユーザでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 現在ログインしているユーザを返す
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # ユーザがログインしていればtrue, その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end
